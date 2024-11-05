3e822aa57fd074b7a5279f3b3f2dd9b15e9dbf82
dmitriystoykov <stoykovdm@gmail.com>
2024-11-05 16:04:56 +0200

    Basic api works

---
+++ my_proj1/books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
+
+# Create your views here.
+class BooksListView(ListView):
+    model = Book
+    template_name = 'books/book-list.html'
